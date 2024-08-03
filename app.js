e9faf258cf789db310f60c60d51e819151222267
arif-mirza <mirzaarif919@gmail.com>
2024-08-03 20:43:16 +0500

    Js concepts

---
+++ app.js
@@ -0,0 +1,7 @@
+// Syntax and Operators
+
+// Variables (let, const, var)
+
+// let name = 'John'; // prefer to use
+// const age = 30;  // for constant and it is not change 
+// var isStudent = true; // it has not globle scope
